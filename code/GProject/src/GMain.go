efc5a22a593168fb89bdba13647679b6c6c31ab0
Gerard KESSE <tiakagerard@hotmail.com>
2020-10-30 19:39:00 +0100

    Initial Commit

--- code/GProject/src/GMain.go
+++ code/GProject/src/GMain.go
@@ -3,23 +3,7 @@ package main
 //===============================================
 import "manager"
 //===============================================
-func GProcess()(*manager.GProcessO) {
+func GProcess() *manager.GProcessO {
     return manager.GProcess()
-}
-//===============================================
-func GProcessUi()(*manager.GProcessUiO) {
-    return manager.GProcessUi()
-}
-//===============================================
-func GManager()(*manager.GManagerO) {
-    return manager.GManager()
-}
-//===============================================
-func GConfig()(*manager.GConfigO) {
-    return manager.GConfig()
-}
-//===============================================
-func GSQLiteUi()(*manager.GSQLiteUiO) {
-    return manager.GSQLiteUi()
 }
 //===============================================
